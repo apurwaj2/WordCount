34d162e97ff6b9ce001d57f23cfb5b7ae83315ba
Apurwa Jadhav <apurwaj2@gmail.com>
2020-04-22 18:11:11 -0700

    Added file ProcessHugeFile and README

---
+++ src/com/company/ProcessHugeFiles.java
@@ -0,0 +1,125 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ProcessHugeFiles {
+
+    private static final int THREAD_COUNT = 8;
+    private static final String EOF = "EOF";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long startTime = System.nanoTime();
+        String file_name = "/home/heisenberg/Downloads/400M";
+        int K = 25;
+
+        /* Function called for reading and processing a file */
+        List list = topKwords(file_name, K);
+        System.out.println(list);
+
+        /* Displaying the time taken for execution */
+        long endTime = System.nanoTime();
+        long elapsedTimeInMillis = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("Total elapsed time: " + elapsedTimeInMillis + " ms");
+
+    }
+
+    public static List topKwords(String file_name, int K) throws IOException, InterruptedException {
+
+        Map<String, Integer> map = new ConcurrentHashMap<String, Integer>();
+        BlockingQueue<String> queue = new LinkedBlockingDeque<>();
+
+        new Thread() {
+            @Override
+            public void run() {
+                int i = 0;
+                try {
+                    /* Reading file initialization */
+                    String line = null;
+                    BufferedReader br = new BufferedReader(new FileReader(file_name));
+                    while ((line = br.readLine()) != null) {
+                        queue.put(line);
+                        i++;
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Value of i " + i);
+            }
+        }.start();
+
+        // Wait for the thread to start writing into the queue
+        Thread.sleep(100);
+
+        ExecutorService es = Executors.newFixedThreadPool(THREAD_COUNT);
+        List<Task> taskList = new ArrayList<>();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Task taskInstance = new Task(map, queue);
+            taskList.add(taskInstance);
+        }
+
+        List<Future<Integer>> result = null;
+
+        try {
+            result = es.invokeAll(taskList);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        es.shutdown();
+        //  es.awaitTermination(5, TimeUnit.SECONDS);
+
+        List<String> ans = new ArrayList();
+
+        PriorityQueue<String> heap;
+        heap = new PriorityQueue<String>(
+                (w1, w2) -> map.get(w1).equals(map.get(w2)) ?
+                        w2.compareTo(w1) : map.get(w1) - map.get(w2));
+
+        for (String word : map.keySet()) {
+            heap.offer(word);
+            if (heap.size() > K) heap.poll();
+        }
+
+        while (!heap.isEmpty()) ans.add(heap.poll());
+        Collections.reverse(ans);
+
+        return ans;
+    }
+
+    private static class Task implements Callable<Integer> {
+
+        private Map<String, Integer> map;
+        private BlockingQueue<String> queue;
+
+        public Task(Map<String, Integer> map, BlockingQueue<String> queue) {
+            this.map = map;
+            this.queue = queue;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            while (true) {
+                String line = null;
+                try {
+                    line = queue.poll(1000, TimeUnit.MILLISECONDS);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String[] words = line.split("\\s+");
+
+                for (String word : words) {
+                    if (!word.isBlank()) {
+                        word.replaceAll("[^a-zA-Z0-9]+", "");
+                        if (!map.containsKey(word)) {
+                            map.putIfAbsent(word, 1);
+                        } else {
+                            map.put(word, map.get(word) + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
